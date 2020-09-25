ca6c3e64bbbfb8d7cf9b7012779a5f3d6db36a96
Naomi Cox <naomicox@mail.weber.edu>
2020-09-24 18:36:43 -0600

    fixed some double scroll ing

--- src/index.js
+++ src/index.js
@@ -7,24 +7,37 @@ import girl from './styles/images/girl.jpeg'
 class App extends Component {
     render() {
         return (
-            <div className="hero">
-                <div className="leftSide">
-                    <img src={girl} className="heroImage"></img>
-                </div>
-                <div className="rightSide">
-                    <div>
-                        <h3> Naomi Cox </h3>
-                        <div className="heroDesc_container">
-                            <p className="heroDesc"> I create <span className="desc_standout">approachable and responsive</span> digital products.
-                        </p>
-                        </div>
-                        <p className="about">About</p>
+            <div>
+                <div className="hero">
+                    <div className="leftSide">
+                        <img src={girl} className="heroImage"></img>
                     </div>
-                    {/* about me div */}
-                    <div><span>hello</span></div>
-                </div> 
+                    <div className="rightSide">
+                        <div>
+                            <h3> Naomi Cox </h3>
+                            <div className="heroDesc_container">
+                                <p className="heroDesc"> I create <span className="desc_standout">approachable and responsive</span> digital products.
+                        </p>
+                            </div>
+                            <p className="about">About</p>
+                        </div>
+                        {/* about me div */}
+                        <div className="aboutMe">
+                            <span>hello this is some filler text to fill the space hello this is some filler text to fill the space hello this is some
+                            filler text to fill the space hello this is some filler text to fill the space
+                            hello this is some filler text to fill the space
+                            hello this is some filler text to fill the space
+                            hello this is some filler text to fill the space
+                            hello this is some filler text to fill the space
+                            hello this is some filler text to fill the space
+                        </span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p>hello text after hero</p>
+                </div>
             </div>
-
         )
     }
 }
